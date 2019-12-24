aa6ad2dd3da2764726c98ed13f3abd05752903fd
PhoeniXWhite <957360688@qq.com>
2019-12-25 02:43:06 +0800

    家里写的

---
+++ iterator_generator_coroutine/fluent_python/17-12_flags2_sequential.py
@@ -0,0 +1,30 @@
+import requests
+
+
+def get_flag(base_url, cc):
+    url = '{}/{cc}/{cc}.gif'.format(base_url, cc=cc.lower())
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        resp.raise_for_status()
+    return resp.content
+
+
+def download_one(cc, base_url, verbose=False):
+    try:
+        image = get_flag(base_url, cc)
+    except requests.exceptions.HTTPError as exc:
+        res = exc.response
+        if res.status_code == 404:
+            status = HTTPStatus.not_found
+            msg = 'not found'
+        else:
+            raise
+    else:
+        save_flag(image, cc.lower() + '.gif')
+        status = HTTPStatus.ok
+        msg = 'ok'
+    
+    if verbose:
+        print(cc, msg)
+    
+    return Result(status, cc)
